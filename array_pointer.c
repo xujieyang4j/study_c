370428630c67a186dc5f38070823336dc82416be
xujieyang <xujieyang@bytedance.com>
2022-11-09 11:40:40 +0800

    strint test

--- array_pointer.c
+++ array_pointer.c
@@ -3,17 +3,19 @@
 void for1();
 void for2();
 void for3();
+void for4();
 int findL1(int a[], int n);
 int findL2(int *a, int n);
 int main(int argc, char const *argv[])
 {
-    int a[N] = {1, 2, 3, 4, 5};
+    // int a[N] = {1, 2, 3, 4, 5};
     
     //for3();
     
-    int lNum = findL2(a, N);
-    printf("lNum:%d\n", lNum);  
+    // int lNum = findL2(a, N);
+    // printf("lNum:%d\n", lNum);  
     
+    for4();
     return 0;
 }
 
@@ -45,6 +47,17 @@ void for3() {
     //  数组名作为指针
     for (p = a + N - 1; p >= a; p--) {
         sum += *p;
+    }
+    printf("sum:%d", sum); 
+}
+
+void for4() {
+    int a[N] = {1, 2, 3, 4, 5};
+    int sum = 0;
+    int *p = a;
+    //  指针作为数组名
+    for (int i = 0; i < N; i++) {
+        sum += p[i];
     }
     printf("sum:%d", sum); 
 }
